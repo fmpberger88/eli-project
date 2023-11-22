f7e2e6150c0a92ca3342060661ff292e38796837
fmpberger88 <fabian.berger@protonmail.ch>
2023-11-22 23:45:44 +0100

    initialize project

---
+++ zlel_web_applikation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for zlel_web_applikation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zlel_web_applikation.settings')
+
+application = get_asgi_application()
---
+++ zlel_web_applikation/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zlel_web_applikation project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zlel_web_applikation.settings')
+
+application = get_wsgi_application()
