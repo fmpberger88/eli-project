93d09ef582f2850e97d26ba2ac27ede5c0e520e5
fmpberger88 <fabian.berger@protonmail.ch>
2023-11-29 23:05:27 +0100

    überarbeitung themenspezifische Informationen

--- zlel_info/urls.py
+++ zlel_info/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path
 from .views import (supplementary_benefits_info,
                     eligibility_requirements,
-                    art_4_detail,
-                    art_5_detail,
-                    art_6_detail,
-                    art_7_detail,
-                    art_8_detail,
-                    art_9_detail,)
+                    el_allgemein,
+                    el_auslaender,
+                    el_vermoegen,
+                    el_einnahmen,
+                    el_ausgaben,
+                    el_kinder,
+                    el_heim,
+                    el_krankheit)
 
 urlpatterns = [
     path('supplementary_benefits_info/', supplementary_benefits_info, name='supplementary_benefits_info'),
     path('eligibility_requirements/', eligibility_requirements, name='eligibility_requirements'),
-    path('art_4_detail/', art_4_detail, name='art_4_detail'),
-    path('art_5_detail/', art_5_detail, name='art_5_detail'),
-    path('art_6_detail/', art_6_detail, name='art_6_detail'),
-    path('art_7_detail/', art_7_detail, name='art_7_detail'),
-    path('art_8_detail/', art_8_detail, name='art_8_detail'),
-    path('art_9_detail/', art_9_detail, name='art_9_detail'),
+    path('el_allgemein/', el_allgemein, name='el_allgemein'),
+    path('el_auslaender/', el_auslaender, name='el_auslaender'),
+    path('el_vermoegen/', el_vermoegen, name='el_vermoegen'),
+    path('el_einnahmen/', el_einnahmen, name='el_einnahmen'),
+    path('el_ausgaben/', el_ausgaben, name='el_ausgaben'),
+    path('el_kinder/', el_kinder, name='el_kinder'),
+    path('el_heim', el_heim, name='el_heim'),
+    path('el_krankheit', el_krankheit, name='el_krankheit')
 ]
--- zlel_info/views.py
+++ zlel_info/views.py
@@ -10,41 +10,26 @@ def supplementary_benefits_info(request):
 def eligibility_requirements(request):
     return render(request, 'zlel_info/eligibility_requirements.html')
 
-def art_4_detail(request):
+def el_allgemein(request):
     return render(request, 'zlel_info/allgemein.html')
 
-def art_5_detail(request):
+def el_auslaender(request):
     return render(request, 'zlel_info/auslaender.html')
 
-def art_6_detail(request):
+def el_vermoegen(request):
     return render(request, 'zlel_info/vermoegen.html')
 
-def art_7_detail(request):
-    return render(request, 'zlel_info/Art7Detail.html')
+def el_einnahmen(request):
+    return render(request, 'zlel_info/Einnahmen.html')
 
-def art_8_detail(request):
-    return render(request, 'zlel_info/Art8Detail.html')
+def el_ausgaben(request):
+    return render(request, 'zlel_info/Ausgaben.html')
 
-def art_9_detail(request):
-    return render(request, 'zlel_info/Art9Detail.html')
+def el_kinder(request):
+    return render(request, 'zlel_info/Kinder.html')
 
-def art_10_detail(request):
-    return render(request, 'zlel_info/Art10Detail.html')
+def el_krankheit(request):
+    return render(request, 'zlel_info/krankheit.html')
 
-def art_11_detail(request):
-    return render(request, 'zlel_info/Art11Detail.html')
-
-def art_12_detail(request):
-    return render(request, 'zlel_info/Art12Detail.html')
-
-def art_13_detail(request):
-    return render(request, 'zlel_info/Art13Detail.html')
-
-def art_14_detail(request):
-    return render(request, 'zlel_info/Art14Detail.html')
-
-def art_15_detail(request):
-    return render(request, 'zlel_info/Art15Detail.html')
-
-def art_16_detail(request):
-    return render(request, 'zlel_info/Art16Detail.html')
+def el_heim(request):
+    return render(request, 'zlel_info/heim.html')
