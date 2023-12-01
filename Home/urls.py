7242367489d8893d06986080e234b1fcafc8a70f
fmpberger88 <fabian.berger@protonmail.ch>
2023-12-01 23:24:39 +0100

    add password reset

--- Home/urls.py
+++ Home/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name="signup"),
     path('registration_confirmation/', views.registration_confirmation, name="registration_confirmation"),
     path('activate/<str:token>', views.activate, name="activate"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
