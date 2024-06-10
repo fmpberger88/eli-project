260e0e2cbff01d90498e57e4759c809eb905b01c
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-10 23:15:56 +0200

    update betreuung im alter

--- static/js/alpine.js
+++ static/js/alpine.js
@@ -44,41 +44,33 @@ document.addEventListener('alpine:init', () => {
         favicons: [
             {
                 "id": "persona1",
-                "url": "./images/persona_1.svg",
-                "name": "Persona 1",
-                "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   \n" +
-                    "\n" +
-                    "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+                "url": "./icons/emma.svg",
+                "name": "Emma, 82 Jahre alt",
+                "description": "Emma ist eine verwitwete Rentnerin, die ihr ganzes Leben in einem kleinen Dorf verbracht hat. Ihre beiden Kinder leben weit entfernt und besuchen sie nur selten. Emma leidet unter leichter Demenz und Arthritis, was ihre Mobilität einschränkt. Sie benötigt emotionale Unterstützung und Hilfe bei alltäglichen Aufgaben wie Einkaufen und Hausarbeit. Da sie sich oft einsam fühlt, wünscht sie sich mehr soziale Kontakte. Die psychosoziale Betreuung umfasst regelmäßige Besuche, Teilnahme an lokalen Seniorengruppen und Mittagstischen sowie Gedächtnistraining."
             },
             {
                 "id": "persona2",
-                "url": "./images/persona_1.svg",
-                "name": "Persona 2",
-                "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   \n" +
-                    "\n" +
-                    "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+                "url": "./icons/hans.svg",
+                "name": "Hans, 76 Jahre alt",
+                "description": "Hans ist ein ehemaliger Lehrer, der nach dem Tod seiner Frau vor zwei Jahren in eine tiefe Depression fiel. Er lebt allein in einer kleinen Wohnung in der Stadt und leidet zudem an Bluthochdruck. Hans braucht Unterstützung bei der Bewältigung seiner Trauer und bei der Pflege seines sozialen Netzwerks. Er hat großes Interesse an Literatur und würde gerne wieder an kulturellen Aktivitäten teilnehmen. Die Betreuung beinhaltet die Begleitung zur Psychotherapie, zu Selbsthilfegruppen für Trauernde sowie zu kulturellen Veranstaltungen."
             },
             {
                 "id": "persona3",
-                "url": "./images/persona_1.svg",
-                "name": "Persona 3",
-                "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   \n" +
-                    "\n" +
-                    "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+                "url": "./icons/marie.svg",
+                "name": "Marie, 69 Jahre alt",
+                "description": "Maria hat keine Familie mehr und lebt alleine in ihrer Wohnung. Früher war sie eine leidenschaftliche Gärtnerin. Sie möchte sich weiterhin nützlich fühlen und ihre Leidenschaft fürs Gärtnern ausüben. Dank der Begleitung zur Teilnahme an Gartenprojekten und regelmässige Gespräche fühlt sie sich wieder gebraucht."
             },
             {
                 "id": "persona4",
-                "url": "./images/persona_1.svg",
-                "name": "Persona 4",
-                "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   \n" +
-                    "\n" +
-                    "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+                "url": "./icons/peter.svg",
+                "name": "Peter, 74 Jahre alt",
+                "description": "Peter ist geschieden und hat keinen Kontakt mehr zu seiner Familie. Seit Kurzem lebt er in einer betreuten Wohneinrichtung. Peter leidet an Diabetes und leichten kognitiven Beeinträchtigungen. Er benötigt Hilfe bei der Verwaltung seiner Krankheit und bei der sozialen Integration in der neuen Wohnumgebung. Da er sich oft isoliert fühlt, hat er Schwierigkeiten, neue Freundschaften zu schließen. Unsere Betreuung umfasst Gesundheitsberatung, soziale Aktivitäten in der Wohneinrichtung und Einzelgespräche mit einem Psychologen."
             },
             {
-                "id": "persona5",
-                "url": "./images/persona_1.svg",
+                "id": "Anna, 78 Jahre alt",
+                "url": "./icons/anna.svg",
                 "name": "Persona 5",
-                "description": "Beschreibung 5"
+                "description": "Anna kam vor 50 Jahren aus Polen in die Schweiz und lebt seitdem in einer kleinen Wohnung in der Stadt. Sie ist verheiratet und gesundheitlich weitgehend stabil, leidet aber unter dem Gefühl der Isolation. Anna fühlt sich oft zuhause eingeschlossen und fremd in der Schweiz, obwohl sie Deutsch spricht. Sie wünscht sich mehr Kontakte zu Menschen mit ähnlichem Hintergrund, um ihre kulturelle Identität zu pflegen und sich weniger isoliert zu fühlen. Dank regelmässigen Treffen mit anderen Migranten, kulturelle Veranstaltungen und Sprachcafés, fühlt sich Anna sozial und kulturell besser integriert."
 
             },
             // Füge hier weitere Personas hinzu...
@@ -87,6 +79,13 @@ document.addEventListener('alpine:init', () => {
             console.log("Vorher ausgewählt:", this.selected);
             this.selected = this.selected === name ? null : name;
             console.log("Nachher ausgewählt:", this.selected);
+        },
+
+        getSelectedName() {
+            console.log("Aktuell ausgewählte Persona:", this.selected);
+            const persona = this.favicons.find(favicon => favicon.name === this.selected);
+            console.log("Gefundene Persona:", persona);
+            return persona ? persona.name : '';
         },
 
         getSelectedDescription() {
